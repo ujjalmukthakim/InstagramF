9377fa686fdcaaed4a18a1d307e65adc62777311
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-01-04 02:43:04 +0600

    ok

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom"
 import { useContext } from "react"
-import { AuthContext } from "../../context/AuthContext"
+// import { AuthContext } from "../../context/AuthContext"
+import {AuthContext} from "../../context/AuthContext"
 
 const Sidebar = () => {
   const { user } = useContext(AuthContext)
